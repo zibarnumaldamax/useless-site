70cb82cec5cd18c9a7d3f980619e3f1aa8bbc6c0
zibarnumaldamax <zibarnumaldamax@gmail.com>
2023-06-10 20:40:48 +0300

    cosmetic changes

--- js/course.js
+++ js/course.js
@@ -6,7 +6,7 @@ const block4 = document.getElementById("block4");
 
 Btn.addEventListener('click', () => {
   block2.innerHTML = ""
-  block2.innerHTML = `<img id="myimg" src="img/kekw.png" alt="картинка" height="300">`
+  block2.innerHTML = `<img id="myimg" src="img/kekw.png" alt="картинка">`
   block3.innerHTML = `<input type="range" min="0" max="100" value="0" id="slider">`
   block4.innerHTML = `<img src="img/couse-pull.png" alt="картинка" height="75">`
 
